e58f23d7ed2629ff3c12581d3c35a469d6bf9380
Tiago Oliveira <tiago-oliviera-1995@hotmail.com>
2019-02-03 14:39:27 +0000

    Bug fixes & training
    Fixed bugs in model.py

--- P2 - Image Captioning/model.py
+++ P2 - Image Captioning/model.py
@@ -13,13 +13,11 @@ class EncoderCNN(nn.Module):
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
-        self.batch = nn.BatchNorm1d(512)
-        
+
     def forward(self, images):
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
-        features = self.batch(features)
         return features
     
 
@@ -28,14 +26,12 @@ class DecoderRNN(nn.Module):
     
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super().__init__()
-        
-        self.embed = nn.Embedding(vocab_size, embed_size)
-       
+               
         self.hidden_size = hidden_size
         
         self.hidden_dim = hidden_size        
         
-        self.embeddings = nn.Embedding(num_embeddings = vocab_size,
+        self.embed = nn.Embedding(num_embeddings = vocab_size,
                                       embedding_dim = embed_size)
         
         self.lstm = nn.LSTM(input_size = embed_size, 
@@ -51,9 +47,9 @@ class DecoderRNN(nn.Module):
        
         captions = captions[:, :-1]  
         
-        embeddings = self.embeddings(captions)
+        embedding = self.embed(captions)
         
-        embedding = torch.cat((features.unsqueeze(1), self.embeddings(captions)), dim=1)
+        embedding = torch.cat((features.unsqueeze(dim = 1), embedding), dim=1)
         
         lstm_out, hidden = self.lstm(embedding) 
         
